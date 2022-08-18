b4727aced89a3a8268d6b5792aa3231c0594553e
kevinzepwa <zepwakevin8@gmail.com>
2022-08-18 10:14:01 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+// Code your solution in this file!
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(anInteger){
+    return function(fare) {
+        return (anInteger * fare)
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayOfDrivers, selectFunction) {
+    return selectFunction(arrayOfDrivers)
+}
